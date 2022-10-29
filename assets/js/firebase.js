ca80708acd785f4fd2eb4f5fbd5c05e426dbd5c0
Guiidtk <guilhermealbuquerque.senai@gmail.com>
2022-10-29 13:27:16 -0300

    first commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9aRRpz6p-IE-Enps462vS18Sta-DcU0k",
+  authDomain: "realtime-copa-2022-5e8e1.firebaseapp.com",
+  projectId: "realtime-copa-2022-5e8e1",
+  storageBucket: "realtime-copa-2022-5e8e1.appspot.com",
+  messagingSenderId: "27053814467",
+  appId: "1:27053814467:web:439e22c5b45ae82878a876"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
